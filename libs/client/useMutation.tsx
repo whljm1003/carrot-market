05edcb8e3bd4cbd8c48e50cc880d067adb0a3030
Jungmin Lee <whljm1003@gmail.com>
2022-08-15 16:02:28 +0900

    6.3 Clean Code part Two

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -9,10 +9,25 @@ interface UseMutationSate {
 type UseMutationResult = [(data: any) => void, UseMutationSate];
 
 export default function useMutation(url: string): UseMutationResult {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<undefined | any>(undefined);
-  const [error, setError] = useState<undefined | any>(undefined);
-  const mutation = (data: any) => {};
+  const [state, setState] = useState<UseMutationSate>({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
+  const mutation = (data: any) => {
+    setState((prev) => ({ ...prev, loading: true }));
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json().catch(() => {}))
+      .then((data) => setState((prev) => ({ ...prev, data })))
+      .catch((error) => setState((prev) => ({ ...prev, error })))
+      .finally(() => setState((prev) => ({ ...prev, loading: false })));
+  };
 
-  return [mutation, { loading, data, error }];
+  return [mutation, { ...state }];
 }
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -13,7 +13,6 @@ interface EnterForm {
 
 const Enter: NextPage = () => {
   const [enter, { loading, data, error }] = useMutation("api/users/enter");
-  const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {
@@ -24,19 +23,11 @@ const Enter: NextPage = () => {
     reset();
     setMethod("phone");
   };
-  const onValid = (data: EnterForm) => {
-    enter(data);
-    // setSubmitting(true);
-    // fetch("/api/users/enter", {
-    //   method: "POST",
-    //   body: JSON.stringify(data),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // }).then(() => {
-    //   setSubmitting(false);
-    // });
+  const onValid = (validForm: EnterForm) => {
+    if (loading) return;
+    enter(validForm);
   };
+  console.log(loading, data, error);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -91,11 +82,11 @@ const Enter: NextPage = () => {
               required
             />
           ) : null}
-          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "email" ? (
+            <Button text={loading ? "Loding" : "Get login link"} />
+          ) : null}
           {method === "phone" ? (
-            <Button
-              text={submitting ? "Loading..." : "Get one-time password"}
-            />
+            <Button text={loading ? "Loading..." : "Get one-time password"} />
           ) : null}
         </form>
 
