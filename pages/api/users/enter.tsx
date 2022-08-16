b06c30a4d5eec30e17c1a424746e086e1d6feca0
Jungmin Lee <whljm1003@gmail.com>
2022-08-16 13:32:27 +0900

    6.4 withHandler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
+import widthHandler from "../../../libs/server/widthHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
   res.status(200).end();
 }
+
+export default widthHandler("POST", handler);
