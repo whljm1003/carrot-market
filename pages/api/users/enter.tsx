dfe0fe3850370b434e47912551b144646c8d0a8e
Jungmin Lee <whljm1003@gmail.com>
2022-08-16 17:04:50 +0900

    7.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,8 +3,61 @@ import client from "@libs/server/client";
 import widthHandler from "@libs/server/widthHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { phone, email } = req.body;
+
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+
+  /**
+  let user;
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log("found it.");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("fonud it.");
+    if (!user) {
+      console.log("Did not find.Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+      console.log(user);
+    }
+  } 
+   */
+
+  return res.status(200).end();
 }
 
 export default widthHandler("POST", handler);
