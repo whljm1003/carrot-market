f1f8c65f341ac8203ee07d9f03acf2fbf39dc876
Jungmin Lee <whljm1003@gmail.com>
2022-08-17 15:20:05 +0900

    7.3 Twilo Setup

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
-import widthHandler from "@libs/server/widthHandler";
+import widthHandler, { ResponseType } from "@libs/server/widthHandler";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -22,49 +26,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
 
-  /**
-  let user;
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("fonud it.");
-    if (!user) {
-      console.log("Did not find.Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-      console.log(user);
-    }
-  } 
-   */
-
-  return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
 export default widthHandler("POST", handler);
