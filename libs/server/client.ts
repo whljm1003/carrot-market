300800f33a30783390de67fb021d08ac4fade82f
Jungmin Lee <whljm1003@gmail.com>
2022-08-25 15:44:28 +0900

    9.0 Product Model

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+  var client: PrismaClient | undefined;
+}
+// 여러개 생기는 client 를 관리 해주기 위함.
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default client;
