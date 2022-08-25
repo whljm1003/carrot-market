475fb70eb9402561ce3440c657a5f8273908dff7
Jungmin Lee <whljm1003@gmail.com>
2022-08-25 14:56:03 +0900

    8.3 SWR

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,19 +1,15 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json())
 
 export default function useUser() {
-  const [user, setUser] = useState();
-  const router = useRouter();
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          // replace: 뒤로가기 버튼에 기록을 남기고 싶지 않을 때 사용.
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+  const {data, error} = useSWR("/api/users/me", fetcher)
+
+
+  // replace: 뒤로가기 버튼에 기록을 남기고 싶지 않을 때 사용.
+  // return router.replace("/enter");
+
+  return data;
 }
