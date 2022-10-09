33d4747ad7ebc2a9bf6280fabb1dbb42bba89083
jungminLee <whljm1003@gmail.com>
2022-10-09 19:38:42 +0900

    10.4 Answer part One

--- pages/api/posts/[id]/index.ts
+++ pages/api/posts/[id]/index.ts
@@ -9,6 +9,7 @@ async function handler(
 ) {
   const {
     query: { id },
+    // session: { user },
   } = req;
   const post = await client.post.findUnique({
     where: {
@@ -43,9 +44,21 @@ async function handler(
       },
     },
   });
+  // const isWondering = Boolean(
+  //   await client.wondering.findFirst({
+  //     where: {
+  //       postId: Number(id),
+  //       userId: user?.id,
+  //     },
+  //     select: {
+  //       id: true,
+  //     },
+  //   })
+  // );
   res.json({
     ok: true,
     post,
+    // isWondering,
   });
 }
 
