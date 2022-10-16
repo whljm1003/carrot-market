e09cb107315ca65909c20ed8f17adf9a17113ac0
jungminLee <whljm1003@gmail.com>
2022-10-16 14:56:02 +0900

    11.1 Reviews

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -5,7 +5,6 @@ import useSWR from "swr";
 export default function useUser() {
   const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
-
   useEffect(() => {
     if (data && !data.ok) {
       // replace: 뒤로가기 버튼에 기록을 남기고 싶지 않을 때 사용.
