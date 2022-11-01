1a987d789d369f43dc6decae217a23644a851e44
jungminLee <whljm1003@gmail.com>
2022-11-01 23:33:10 +0900

    12.1 Detail Page

--- pages/api/streams/[id].ts
+++ pages/api/streams/[id].ts
@@ -1,8 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
 
-async function handle(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const {
     query: { id },
   } = req;
-  // const stream = await client.stream;
+  const stream = await client.stream.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json({ ok: true, stream });
 }
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
