e875bfa59f43c99b7a33f3d9b97381442b9dad07
jungminLee <whljm1003@gmail.com>
2022-11-26 15:41:09 +0900

    13.4 Direct Upload URL

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -6,10 +6,24 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  // v2로 바꿔서 내용 바꿔야 할지도 모름.
+  // 현재 유료 결제 안해서 토큰 값 없음.
+  const response = await (
+    await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_API_TOKEN}/images/v1/direct_upload`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CLOUDFLARE_ACCOUNT_ID}`,
+        },
+      }
+    )
+  ).json();
+  console.log(response);
   res.json({
     ok: true,
-    // CF에서 받은 빈 URL
-    url: "",
+    ...response.result,
   });
 }
 
